06d447d1a99e542a0dbc59cbc57926baf56e2916
Quế Tùng <47247560+ZoeMeow5466@users.noreply.github.com>
2022-11-21 09:53:16 +0700

    Remove search bar from home tab

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -13,28 +13,12 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   late CurrentState _currentState;
-  late List<NavigationDestination> navBtnList;
   late List<Widget> stateWidgetList;
 
   @override
   void initState() {
     super.initState();
     _currentState = CurrentState();
-
-    navBtnList = <NavigationDestination>[
-      const NavigationDestination(
-        icon: Icon(Icons.home),
-        label: 'Home',
-      ),
-      const NavigationDestination(
-        icon: Icon(Icons.favorite),
-        label: 'Wishlist',
-      ),
-      const NavigationDestination(
-        icon: Icon(Icons.account_circle),
-        label: 'Account',
-      ),
-    ];
 
     stateWidgetList = [
       const HomeTab(),
@@ -49,11 +33,27 @@ class _HomeViewState extends State<HomeView> {
       body: stateWidgetList[_currentState.currentPage],
       bottomNavigationBar: NavigationBarTheme(
         data: Theme.of(context).navigationBarTheme,
-        child: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [],
+        child: NavigationBarTheme(
+          data: Theme.of(context).navigationBarTheme,
+          child: NavigationBar(
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.favorite),
+                label: 'Wishlist',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.account_circle),
+                label: 'Account',
+              ),
+            ],
+            selectedIndex: _currentState.currentPage,
+            onDestinationSelected: (index) => {
+              setState(() => {_currentState.currentPage = index})
+            },
           ),
         ),
       ),
