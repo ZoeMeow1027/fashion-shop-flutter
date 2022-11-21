52131346b2369deeb6086dce4e29db32b78fc249
Quế Tùng <47247560+ZoeMeow5466@users.noreply.github.com>
2022-11-21 19:23:00 +0700

    Login, home view (with account) and more (#8)
    # Improvements:
    - Login is working now.
    - Account tab will trigger user is logged in or not (UI is not developed yet).

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,52 +1,53 @@
-import 'package:fashionshop/repository/user_api.dart';
-import 'package:fashionshop/screens/home/components/current_state.dart';
+import 'dart:developer';
+
+import 'package:fashionshop/screens/account_login/login_view.dart';
 import 'package:fashionshop/screens/home/views/account_tab.dart';
 import 'package:fashionshop/screens/home/views/home_tab.dart';
 import 'package:fashionshop/screens/home/views/wishlist_tab.dart';
+import 'package:fashionshop/view_model/view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeView extends StatefulWidget {
-  const HomeView({super.key});
+  const HomeView({super.key, required this.viewModel});
+
+  final ViewModel viewModel;
 
   @override
   State<StatefulWidget> createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
-  late CurrentState _currentState;
-  late List<Widget> stateWidgetList;
-
-  String? tokenKey;
+  Object objKey = Object();
+  List<Widget> _widgetList() => [
+        HomeTab(),
+        WishlistTab(),
+        AccountTab(
+          key: ValueKey<Object>(objKey),
+          userProfile: widget.viewModel.userProfile,
+          loginRequested: () async {
+            await Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginView()),
+            );
+            log("triggered");
+            await widget.viewModel.checkLoggedIn();
+            setState(() {
+              objKey = Object();
+            });
+          },
+        ),
+      ];
 
   @override
   void initState() {
     super.initState();
-    _currentState = CurrentState();
-
-    stateWidgetList = [
-      const HomeTab(),
-      const WishlistTab(),
-      const AccountTab(),
-    ];
-
-    SharedPreferences.getInstance().then(
-      (value) => {
-        tokenKey = value.getString("tokenKey"),
-        () async {
-          final isLoggedIn = await UserAPI.isLoggedIn(tokenKey!);
-          if (isLoggedIn) {
-            value.remove("tokenKey");
-          }
-        },
-      },
-    );
+    widget.viewModel.checkLoggedIn();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: stateWidgetList[_currentState.currentPage],
+      body: _widgetList()[widget.viewModel.currentPage],
       bottomNavigationBar: NavigationBarTheme(
         data: Theme.of(context).navigationBarTheme,
         child: NavigationBarTheme(
@@ -66,9 +67,9 @@ class _HomeViewState extends State<HomeView> {
                 label: 'Account',
               ),
             ],
-            selectedIndex: _currentState.currentPage,
+            selectedIndex: widget.viewModel.currentPage,
             onDestinationSelected: (index) => {
-              setState(() => {_currentState.currentPage = index})
+              setState(() => {widget.viewModel.currentPage = index})
             },
           ),
         ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fashionshop/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MainApplication(
+    await tester.pumpWidget(MainApplication(
       welcomePassed: false,
     ));
 
