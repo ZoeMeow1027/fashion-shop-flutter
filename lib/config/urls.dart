d4997fb35c5b33e48ae72041950fc635e175eb87
Quế Tùng <47247560+ZoeMeow5466@users.noreply.github.com>
2022-12-20 10:19:42 +0700

    Change and move all to config/urls.dart

---
+++ lib/config/urls.dart
@@ -0,0 +1,9 @@
+class Urls {
+  static String urlBase = "http://127.0.0.1:8000";
+  static String urlMyOrders = "/api/orders/myorders/";
+  static String urlGetProducts = "/api/products/";
+  static String urlProfile = "/api/users/profile/";
+  static String urlProfileUpdate = "/api/users/profile/update/";
+  static String urlLogin = "/api/users/login/";
+  static String urlRegister = "/api/users/register/";
+}
